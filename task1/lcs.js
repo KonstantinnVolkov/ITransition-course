9edc9775bcc3b10cdb33020efb628f69244a542f
Konstantin <volkovkonstantinn@gmail.com>
2022-02-07 19:19:14 +0300

    1st task completed

--- task1/lcs.js
+++ task1/lcs.js
@@ -1,5 +1,25 @@
 const [ node, file, ...args ] = process.argv;
-const string = args.join(' ');
-console.log(string);
 
-console.log()
+let n = args.length;
+if (n == 0) {
+    console.log("")
+}
+else {
+    let firstWord = args[0];
+    let firstWordLen = firstWord.length;
+    let res = "";
+
+    for (let i = 0; i < firstWordLen; i++) {
+        for (let j = i + 1; j <= firstWordLen; j++) {
+            let substring = firstWord.substring(i, j);
+            let k = 1;
+            for (k = 1; k < n; k++) {
+                if (!args[k].includes(substring))
+                    break;
+            }
+            if (k == n && res.length < substring.length)
+                res = substring;
+        }
+    }
+    console.log(res);
+}
