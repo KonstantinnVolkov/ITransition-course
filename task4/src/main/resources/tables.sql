27dc30b2dbd38ec7de1767ec01d6c596f7654b56
konstantinnVolkov <volkovkonstantinn@gmail.com>
2022-04-27 16:37:57 +0300

    final commit

--- task4/src/main/resources/tables.sql
+++ task4/src/main/resources/tables.sql
@@ -1,18 +1,18 @@
 CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       username VARCHAR(255) NOT NULL UNIQUE,
-                       email VARCHAR(255) NOT NULL UNIQUE,
-                       password VARCHAR(255) NOT NULL,
-                       regDate TIMESTAMP,
-                       lastLogin TIMESTAMP,
-                       role VARCHAR(25) NOT NULL,
-                       status VARCHAR(25) NOT NULL
+   id SERIAL PRIMARY KEY,
+   username VARCHAR(255) NOT NULL UNIQUE,
+   email VARCHAR(255) NOT NULL UNIQUE,
+   password VARCHAR(255) NOT NULL,
+   regDate TIMESTAMP,
+   lastLogin TIMESTAMP,
+   role VARCHAR(25) NOT NULL,
+   status VARCHAR(25) NOT NULL
 );
 
 CREATE TABLE messages (
-                          id SERIAL PRIMARY KEY,
-                          sender VARCHAR(255) NOT NULL,
-                          receiver VARCHAR(255) NOT NULL,
-                          theme TEXT NOT NULL,
-                          text TEXT NOT NULL
+    id SERIAL PRIMARY KEY,
+    sender VARCHAR(255) NOT NULL,
+    receiver VARCHAR(255) NOT NULL,
+    theme TEXT NOT NULL,
+    text TEXT NOT NULL
 );
